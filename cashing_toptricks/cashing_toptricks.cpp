1fec818e5840ed6408768d80ebfdce2a31d00145
Varshith Bathini <varshith15@gmail.com>
2020-02-25 01:47:54 +0530

    optsearch

--- cashing_toptricks/cashing_toptricks.cpp
+++ cashing_toptricks/cashing_toptricks.cpp
@@ -21,6 +21,7 @@ class cashing{
 	void input();
 	void rank();	
 	void search();
+	void optsearch();
 };
 
 void cashing::input(){
@@ -264,6 +265,146 @@ void cashing::search(){
 	cout<<"max top tricks: "<<ma<<endl;
 	cout<<line_of_play;
 }
+
+void cashing::optsearch(){
+	int ma=INT_MIN;
+	queue<nd> q;
+	nd temp;
+	temp.p=1;
+	temp.t=0;
+	temp.r=0;
+	temp.play="";
+	for(int i=0;i<4;i++){
+		temp.north[i]=north[i];
+		temp.south[i]=south[i];
+		for(int j=0;j<north[i].size();j++){
+			if(north[i][j].second==1){
+				temp.r++;
+			}
+		}
+		for(int j=0;j<south[i].size();j++){
+			if(south[i][j].second==1){
+				temp.r++;
+			}	
+		}
+	}
+	q.push(temp);
+	string line_of_play="";
+	while(!q.empty()){
+		nd temp = q.front();
+		q.pop();
+		if(ma<=temp.t){
+			ma=temp.t;
+			line_of_play=temp.play;
+		}
+		if(temp.r==0){
+			cout<<temp.t<<endl;
+			continue;
+		}
+		if(temp.p==1){
+			for(int i=0;i<4;i++){
+				if(temp.south[i].size()==0){
+					continue;
+				}
+				if(temp.north[i].size()==0){
+					for(int j=0;j<4;j++){
+						if(temp.north[j].size()==0){
+							continue;
+						}
+						if(temp.south[i][0].second==1){
+							nd temp1 = temp;
+							temp1.p=1;
+							temp1.r=temp.r-(temp.south[i][0].second==1)-(temp.north[j][0].second==1);
+							temp1.play = temp.play+"South plays "+to_string(temp.south[i][0].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[j][0].first)+" "+to_string(j+1)+"\n";
+							temp1.south[i].erase(temp1.south[i].begin());
+							temp1.north[j].erase(temp1.north[j].begin());
+							q.push(temp1);
+							if(temp.north[j].size()==1){
+								continue;
+							}
+							int la=temp.north[j].size();
+							temp1=temp;
+							temp1.p=1;
+							temp1.r=temp.r-(temp.south[i][0].second==1)-(temp.north[j][la-1].second==1);
+							temp1.play = temp.play+"South plays "+to_string(temp.south[i][0].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[j][la-1].first)+" "+to_string(j+1)+"\n";
+							temp1.south[i].erase(temp1.south[i].begin());
+							temp1.north[j].erase(temp1.north[j].begin()+la-1);
+							q.push(temp1);
+						}
+					}
+					if(temp.south[i].size()==1){
+						continue;
+					}
+					for(int j=0;j<4;j++){
+						int ls=temp.south[i].size();
+						if(temp.north[j].size()==0){
+							continue;
+						}
+						if(temp.south[i][ls-1].second==1){
+							nd temp1 = temp;
+							temp1.p=1;
+							temp1.r=temp.r-(temp.south[i][ls-1].second==1)-(temp.north[j][0].second==1);
+							temp1.play = temp.play+"South plays "+to_string(temp.south[i][ls-1].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[j][0].first)+" "+to_string(j+1)+"\n";
+							temp1.south[i].erase(temp1.south[i].begin()+ls-1);
+							temp1.north[j].erase(temp1.north[j].begin());
+							q.push(temp1);
+							if(temp.north[j].size()==1){
+								continue;
+							}
+							int la=temp.north[j].size();
+							temp1=temp;
+							temp1.p=1;
+							temp1.r=temp.r-(temp.south[i][ls-1].second==1)-(temp.north[j][la-1].second==1);
+							temp1.play = temp.play+"South plays "+to_string(temp.south[i][ls-1].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[j][la-1].first)+" "+to_string(j+1)+"\n";
+							temp1.south[i].erase(temp1.south[i].begin()+ls-1);
+							temp1.north[j].erase(temp1.north[j].begin()+la-1);
+							q.push(temp1);
+						}
+					}
+				}
+				else{
+					if(temp.south[i][0].second==1 || temp.north[i][0].second==1){
+						nd temp1 = temp;
+						temp1.p = temp.south[i][0].first>temp.north[i][0].first;
+						temp1.r = temp.r-(temp.south[i][0].second==1)-(temp.north[i][0].second==1);
+						temp1.play = temp.play+"South plays "+to_string(temp.south[i][0].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[i][0].first)+" "+to_string(i+1)+"\n";
+						temp1.south[i].erase(temp1.south[i].begin());
+						temp1.north[i].erase(temp1.north[i].begin());
+						q.push(temp1);
+					}
+					if(temp.south[i].size()>1 && (temp.south[i][temp.south[i].size()-1].second==1 || temp.north[i][0].second==1)){
+						nd temp1 = temp;
+						temp1.p = temp.south[i][temp.south[i].size()-1].first>temp.north[i][0].first;
+						temp1.r = temp.r-(temp.south[i][temp.south[i].size()-1].second==1)-(temp.north[i][0].second==1);
+						temp1.play = temp.play+"South plays "+to_string(temp.south[i][temp.south[i].size()-1].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[i][0].first)+" "+to_string(i+1)+"\n";
+						temp1.south[i].erase(temp1.south[i].begin()+temp.south[i].size()-1);
+						temp1.north[i].erase(temp1.north[i].begin());
+						q.push(temp1);
+					}
+					if(temp.north[i].size()>1 && (temp.south[i][0].second==1 || temp.north[i][temp.north[i].size()-1].second==1)){
+						nd temp1 = temp;
+						temp1.p = temp.south[i][0].first>temp.north[i][temp.north[i].size()-1].first;
+						temp1.r = temp.r-(temp.south[i][0].second==1)-(temp.north[i][temp.north[i].size()-1].second==1);
+						temp1.play = temp.play+"South plays "+to_string(temp.south[i][0].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[i][temp.north[i].size()-1].first)+" "+to_string(i+1)+"\n";
+						temp1.south[i].erase(temp1.south[i].begin());
+						temp1.north[i].erase(temp1.north[i].begin()+temp.north[i].size()-1);
+						q.push(temp1);
+					}
+					if(temp.south[i].size()>1 && temp.north[i].size()>1 && (temp.south[i][temp.south[i].size()-1].second==1 || temp.north[i][temp.north[i].size()-1].second==1)){
+						nd temp1 = temp;
+						temp1.p = temp.south[i][temp.south[i].size()-1].first>temp.north[i][temp.north[i].size()-1].first;
+						temp1.r = temp.r-(temp.south[i][temp.south[i].size()-1].second==1)-(temp.north[i][temp.north[i].size()-1].second==1);
+						temp1.play = temp.play+"South plays "+to_string(temp.south[i][temp.south[i].size()-1].first)+" "+to_string(i+1)+"\nNorth plays "+to_string(temp.north[i][temp.north[i].size()-1].first)+" "+to_string(i+1)+"\n";
+						temp1.south[i].erase(temp1.south[i].begin()+temp.south[i].size()-1);
+						temp1.north[i].erase(temp1.north[i].begin()+temp.north[i].size()-1);
+						q.push(temp1);
+					}
+				}
+			}
+		}
+	}
+}
+
 
 int main(){
 	cashing c;
